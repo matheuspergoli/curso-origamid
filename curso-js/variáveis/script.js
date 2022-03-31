d763ec48b9908ea340395cc7ff796380f9a5fac2
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-31 10:17:28 -0300

    aula

---
+++ curso-js/variáveis/script.js
@@ -0,0 +1,34 @@
+var nome = 'Matheus'
+var idade = 22
+var possuiFaculdade = false
+
+console.log(nome, idade, possuiFaculdade)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco)
+
+var sobrenome = 'Pergoli', 
+    cidade = 'São paulo'
+
+console.log(sobrenome, cidade)
+
+var semDefinir
+console.log(semDefinir)
+
+var time = 'Vasco'
+time = 'Flamengo'
+console.log(time)
+
+const meuNome = 'Matheus'
+const minhaIdade = 22
+let comidaFavorita
+comidaFavorita = 'Lentilha'
+
+let meuTime, 
+    professor, 
+    pais, 
+    endereco, 
+    rua
