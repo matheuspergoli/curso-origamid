b6731c7d5601e166c06ff1f72d6dd7848d9fff58
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-31 17:04:42 -0300

    aula

--- curso-js/boolean-e-condicionais/script.js
+++ curso-js/boolean-e-condicionais/script.js
@@ -20,7 +20,50 @@ if (nome) {
 /*
 Esses dois sinais de exclamação serve para verificar se um valor é truthy ou falsy
 */
-console.log(!!' ')
-console.log(!!'')
-console.log(!!5)
-console.log(!!0)
+
+var nome1 = 'André'
+var idade = 28
+var possuiDoutorado = false
+var empregoFuturo
+var dinheiroNaConta = 0
+
+console.log(!!nome1)
+console.log(!!idade)
+console.log(!!possuiDoutorado)
+console.log(!!empregoFuturo)
+console.log(!!dinheiroNaConta)
+
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2)
+console.log(expressao)
+
+var brasil = 207
+var china = 1340
+
+if (brasil > china) {
+  console.log('População do Brasil é maior que a da China')
+} else {
+  console.log('População da China é maior que a do Brasil')
+}
+
+const corFavorita = 'Roxo'
+
+switch (corFavorita) {
+  case 'Azul':
+    console.log('Olhe para o céu')
+    break
+
+  case 'Vermelho':
+    console.log('Olhe para as rosas')
+    break
+
+  case 'Amarelo':
+    console.log('Olhe para o sol')
+    break
+  
+  case 'Verde':
+    console.log('Olhe para a floresta')
+    break
+
+  default:
+    console.log('Feche os olhos')
+}
