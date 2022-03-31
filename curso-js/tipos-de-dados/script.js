9c292a28f145e04fc0f3f0c98f66a35472be50cb
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-31 14:27:20 -0300

    aula

---
+++ curso-js/tipos-de-dados/script.js
@@ -0,0 +1,19 @@
+var nome = 'Matheus'
+var sobrenome = 'Pergoli'
+var nomeCompleto = nome + ' ' + sobrenome
+var idade = 22
+var time = null
+var simbolo = Symbol()
+var gols = 1000
+var frase = 'Romário fez ' + gols + ' gols'
+var frase1 = 'Esse é o \'melhor\' jogo'
+var frase2 = `Romário fez ${gols * 2} gols`
+
+var variavelString = 'Essa é uma String'
+var numeroString = '2022'
+var idade2 = 22
+var meuNome = 'Matheus'
+var meuSobrenome = 'Pergoli'
+var meuNomeCompleto = meuNome + ' ' + meuSobrenome
+var teste = 'It\'s time'
+console.log(typeof meuNome)
