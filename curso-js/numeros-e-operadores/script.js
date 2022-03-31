dc8b4b9421234cf4442185f367cd0147c5d7b69b
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-31 14:53:08 -0300

    aula

---
+++ curso-js/numeros-e-operadores/script.js
@@ -0,0 +1,24 @@
+var soma = 100 + 50 // 150
+var subtracao = 100 - 50 // 50
+var multiplicacao = 100 * 2 // 200
+var divisao = 100 / 2 // 50
+var expoente = 2 ** 4 // 16
+var modulo = 14 % 5 // 4
+var teste = 'Comprei 10' / 2
+var soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10
+var incremento = 1
+var idade = '22'
+var somaIdade = 5
+var total = 10 + 5 * 2 / 2 + 20
+var teste1 = 'Matheus'
+var teste2 = 22
+var testeNan = +teste1 + teste2
+var testeNan2 = total + testeNan
+var somar200 = '200'
+var incremento2 = 5
+var numero = '80'
+var unidade = 'kg'
+var peso = +numero
+var pesoPorDois = +peso / 2
+
+console.log(pesoPorDois)
