fd29187d4021632318c2d650e207d8bcd55a1f45
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-01 11:30:06 -0300

    aula

---
+++ curso-js/atribuicao-e-ternario/script.js
@@ -0,0 +1,21 @@
+var numero = 20
+var numero2 = 10
+
+numero += numero2 // numero = numero + 10
+console.log(numero)
+
+var idade = 16
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber'
+console.log(podeBeber)
+
+var possuiFaculdade = true
+if (possuiFaculdade) console.log('Sim possui')
+
+var scroll = 1000
+scroll += 500
+console.log(scroll)
+
+var possuiCarro = true
+var possuiCasa = true
+var darCredito = (possuiCarro && possuiCasa)
+console.log(darCredito)
