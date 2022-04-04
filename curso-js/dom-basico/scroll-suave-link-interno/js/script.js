23903bdc514f20cff74bc2271f48a35b182b0781
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-04 02:03:58 -0300

    aula

--- curso-js/dom-basico/scroll-suave-link-interno/js/script.js
+++ curso-js/dom-basico/scroll-suave-link-interno/js/script.js
@@ -39,26 +39,29 @@ function initAccordion() {
 }
 initAccordion()
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
-
-function scrollToSection(event) {
-  event.preventDefault()
-  const href = event.currentTarget.getAttribute('href')
-  const section = document.querySelector(href)
-
-  section.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+  
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+  
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  
+    // Forma alternativa
+    // const topo = section.offsetTop
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth'
+    // })
+  }
+  
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToSection)
   })
-
-  // Forma alternativa
-  // const topo = section.offsetTop
-  // window.scrollTo({
-  //   top: topo,
-  //   behavior: 'smooth'
-  // })
 }
-
-linksInternos.forEach(link => {
-  link.addEventListener('click', scrollToSection)
-})
+initScrollSuave()
