97cbbfac769694c46022f4e91173b3b8f8e84d2a
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-10 21:31:46 -0300

    aula

---
+++ curso-js/objetos/string/script.js
@@ -0,0 +1,29 @@
+const comida = 'Pizza'
+const agua = new String('Agua')
+const frase = 'A melhor linguagem é'
+const linguagem = 'JavaScript'
+const fraseFinal = frase.concat(linguagem, '!!')
+const fruta = 'Banana'
+const listaFrutas = 'Melancia, Banana, Laranja'
+const transacao1 = 'Depósito de cliente'
+const transacao2 = 'Depósito de fornecedor'
+const transacao3 = 'Taxa da camisas'
+const valor = 'R$ 99'
+const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000']
+
+// console.log(comida.length)
+// console.log(agua.length)
+// console.log(frase.charAt(0))
+// console.log(frase[frase.length - 1])
+// console.log(frase + linguagem)
+// console.log(fraseFinal)
+// console.log(listaFrutas.includes(fruta))
+// console.log(fruta.startsWith('Ba'))
+// console.log(fruta.endsWith('na'))
+// console.log(transacao1.slice(0, -1))
+// console.log(linguagem.substring(0, 4))
+// console.log(fruta.lastIndexOf('n'))
+// console.log(valor.padStart(6, '.'))
+listaPrecos.forEach(item => {
+  console.log(item.padStart())
+})
