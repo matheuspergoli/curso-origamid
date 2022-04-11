8e687a9d6ff310bd74e34a9783969af869f4f997
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-11 02:15:53 -0300

    aula

--- curso-js/objetos/string/script.js
+++ curso-js/objetos/string/script.js
@@ -8,8 +8,18 @@ const listaFrutas = 'Melancia, Banana, Laranja'
 const transacao1 = 'Depósito de cliente'
 const transacao2 = 'Depósito de fornecedor'
 const transacao3 = 'Taxa da camisas'
-const valor = 'R$ 99'
+const valor = ' R$ 99 '
 const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000']
+let listaItens = 'Camisas Bonés Calças Bermudas Vestidos Saias'
+listaItens = listaItens.replace(/[ ]+/g, ', ')
+const arrayLista = listaItens.split(', ')
+const htmlText = '<div>O melhor item</div><div>A melhor lista</div>'
+const htmlArray = htmlText.split('div')
+const novoHtml = htmlArray.join('section')
+const frutasArray = ['Banana', 'Melancia', 'Laranja']
+const sexo1 = 'Feminino'
+const sexo2 = 'feminino'
+const sexo3 = 'FEMININO'
 
 // console.log(comida.length)
 // console.log(agua.length)
@@ -24,6 +34,13 @@ const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000']
 // console.log(linguagem.substring(0, 4))
 // console.log(fruta.lastIndexOf('n'))
 // console.log(valor.padStart(6, '.'))
-listaPrecos.forEach(item => {
-  console.log(item.padStart())
-})
+// listaPrecos.forEach(item => {
+//   console.log(item.padStart())
+// })
+// console.log(comida.repeat(5))
+// console.log(listaItens)
+// console.log(arrayLista)
+// console.log(htmlArray)
+// console.log(novoHtml)
+// console.log(sexo3.toLowerCase() === 'feminino')
+// console.log(valor.trimStart())
