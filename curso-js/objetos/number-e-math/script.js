9b9c4c92f95a461ac7f36e88c814c8a2b906c624
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-12 16:05:14 -0300

    aula

---
+++ curso-js/objetos/number-e-math/script.js
@@ -0,0 +1,27 @@
+// console.log(Number.isNaN(5))
+// console.log(Number.isInteger(50))
+// console.log(parseFloat('12345.6789'))
+// console.log(parseFloat('100 Reais'))
+// console.log(parseInt('150.50'))
+// console.log(parseInt(50))
+
+// const preco = 10.25
+// console.log(preco.toFixed())
+// console.log((99).toFixed(1))
+// console.log(preco.toString())
+// console.log(preco.toLocaleString('pt-BR', {
+//   style: 'currency',
+//   currency: 'BRL'
+// }))
+// console.log(preco.toLocaleString('en-US', {style: 'currency', currency: 'USD'}))
+// console.log(Math.PI)
+// console.log(Math.abs(-55))
+// console.log(Math.ceil(5.49))
+// console.log(Math.floor(5.49))
+// console.log(Math.round(5.49))
+// console.log(Math.round(5.51))
+// console.log(Math.max(5, 55, 20, 1000, 5000, 35000))
+// console.log(Math.min(5, 10, 2, 15, 1))
+// console.log(Math.random())
+// const random = Math.floor(Math.random() * (100 - 1) + 1)
+// console.log(random)
