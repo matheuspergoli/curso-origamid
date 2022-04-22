1c3db795e3fa60410f7661701efb9eace809b54f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-22 10:09:09 -0300

    Aula

--- curso-js/objetos/function/script.js
+++ curso-js/objetos/function/script.js
@@ -1,58 +1,42 @@
-// const perimetro = new Function('lado', 'return lado * 4')
+const perimetro = new Function('lado', 'return lado * 4')
 
-// function somar(n1, n2) {
-//   return n1 + n2
-// }
-// console.log(somar.name)
+function somar(n1, n2) {
+  return n1 + n2
+}
 // console.log(somar.length)
+// console.log(somar.name)
+// console.log(somar.name.toUpperCase())
 
-// function darOi(nome) {
-//   console.log('Oi pra você ' + nome)
-// }
+function darOi(nome, idade) {
+  console.log(`Oi pra você ${nome} | Idade: ${idade}`)
+}
+// darOi.call({}, 'Matheus', 22)
 
-// darOi.call({}, 'Matheus')
+const carro = {
+  marca: 'Ford',
+  ano: 2018
+}
 
-// function descricaoCarro(velocidade) {
-//   console.log(this)
-//   console.log(this.marca + ' ' + this.ano + ' ' + velocidade)
-// }
+function descricaoCarro() {
+  console.log(this)
+  console.log(this.marca + ' ' + this.ano)
+}
+// descricaoCarro.call({marca: 'Ferrari', ano: 2018})
 
-// descricaoCarro.call({marca: 'Honda', ano: 2015}, 100)
+const carros = ['Ford', 'Fiat', 'VW']
+const frutas = ['Banana', 'Uva', 'Pêra']
 
-// const carros = ['Ford', 'Fiat', 'VW']
-// const frutas = ['Banana', 'Uva', 'Pêra']
+frutas.forEach.call(carros, carro => {
+  // console.log(carro)
+})
 
-// carros.forEach.call(carros, (item) => {
-  // console.log(item)
-// })
+function Dom(seletor) {
+  this.element = document.querySelector(seletor)
+}
 
-// function Dom(seletor) {
-//   this.element = document.querySelector(seletor)
-// }
+Dom.prototype.ativo = (classe) => {
+  this.element.classList.add(classe)
+}
 
-// Dom.prototype.ativo = function(classe) {
-//   this.element.classList.add(classe)
-// }
-
-// const ul = new Dom('ul')
-
-// const li = {
-//   element: document.querySelector('li')
-// }
-
-// ul.ativo.call(li, 'ativar')
-
-// console.log(ul)
-
-// const frutas = ['Uva', 'Maçã', 'Banana']
-
-// Array.prototype.pop.call(frutas)
-
-// const li = document.querySelectorAll('li')
-
-// const filtro = Array.prototype.filter.call(li, (item) => {
-//   return item.classList.contains('ativo')
-// })
-// console.log(filtro)
-
-const numeros = [33, 232, 33, 434, 54, 5424, 4]
+const ul = new Dom('ul')
+ul.ativo('ativar')
