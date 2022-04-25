1cf629e60447ccf5fbb103fae9c54cb82b934f07
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-24 23:46:09 -0300

    aula

--- curso-js/objetos/object/script.js
+++ curso-js/objetos/object/script.js
@@ -3,30 +3,30 @@ const pessoa = new Object({
 })
 // console.log(pessoa)
 
-const carro = {
-  // marca: 'Marca',
-  init(valor) {
-    this.marca = valor
-    return this
-  },
+// const carro = {
+//   marca: 'Marca',
+//   init(valor) {
+//     this.marca = valor
+//     return this
+//   },
 
-  rodas: 4,
+//   rodas: 4,
 
-  acelerar() {
-    return `${this.marca} acelerou`
-  },
+//   acelerar() {
+//     return `${this.marca} acelerou`
+//   },
 
-  buzinar() {
-    return `${this.marca} buzinou`
-  }
-}
+//   buzinar() {
+//     return `${this.marca} buzinou`
+//   }
+// }
 // console.log(carro)
 
-const honda = Object.create(carro).init('Honda')
+// const honda = Object.create(carro).init('Honda')
 // console.log(honda)
 // console.log(honda.acelerar())
 
-const ferrari = Object.create(carro).init('Ferrari')
+// const ferrari = Object.create(carro).init('Ferrari')
 // console.log(ferrari)
 
 const funcaoAutomovel = {
@@ -51,6 +51,8 @@ const moto = {}
 
 Object.defineProperties(moto, {
   rodas: {
+    enumerable: true,
+
     get() {
       return this._rodas
     },
@@ -63,4 +65,27 @@ Object.defineProperties(moto, {
 
 moto.rodas = 4
 delete moto.rodas
-console.log(moto.rodas)
+// console.log(moto.rodas)
+
+// console.log(Object.getOwnPropertyDescriptors(String.prototype))
+Object.getOwnPropertyDescriptors(Array.prototype)
+Object.getOwnPropertyDescriptor(window, 'innerHeight')
+// console.log(Object.getOwnPropertyNames(Array.prototype))
+// console.log(Object.getOwnPropertyNames(moto))
+
+const frutas = ['Banana']
+// console.log(Object.getPrototypeOf(frutas))
+
+const frutas1 = ['Banana', 'Pêra']
+const frutas2 = ['Banana', 'Pêra']
+
+// console.log(Object.is(frutas1, frutas2))
+
+const carro = {
+  marca: 'Ford',
+  ano: 2018
+}
+Object.freeze(carro)
+carro.marca = 'Honda'
+console.log(carro)
+console.log(Object.isFrozen(carro))
