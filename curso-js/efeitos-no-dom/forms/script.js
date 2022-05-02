baf2ed95050c9d5ee8f1afc98bef7ec62c70ada0
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-02 16:28:03 -0300

    aula

--- curso-js/efeitos-no-dom/forms/script.js
+++ curso-js/efeitos-no-dom/forms/script.js
@@ -1,15 +1,36 @@
 const contato = document.querySelector('#contato')
 const texto = document.querySelector('.texto')
 
-function handleKeyUp(event) {
-  const target = event.target
-  if (!target.checkValidity()) {
-    target.classList.add('invalido')
-    contato.nome.setCustomValidity('Preencha com seu nome.')
-    contato.email.setCustomValidity('Preencha com seu email.')
-    target.nextElementSibling.innerText = target.validationMessage
-  }
-  texto.innerHTML = target.value
+// function handleKeyUp(event) {
+//   const target = event.target
+//   if (!target.checkValidity()) {
+//     target.classList.add('invalido')
+//     contato.nome.setCustomValidity('Nome obrigatório.')
+//     contato.email.setCustomValidity('Insira um email válido.')
+//     target.nextElementSibling.innerText = target.validationMessage
+//   }
+//   contato.mensagem.innerText = target.value
+// }
+// contato.addEventListener('keyup', handleKeyUp)
+
+// function handleKeyUp(event) {
+//   document.body.style.backgroundColor = event.target.value
+// }
+
+// contato.addEventListener('change', handleKeyUp)
+
+// function handleKeyUp(event) {
+//   if (event.target.checked) {
+//     console.log(event.target.value)
+//   }
+// }
+
+// contato.addEventListener('change', handleKeyUp)
+
+const dados = {}
+
+function handleChange(event) {
+  dados[event.target.name] = event.target.value
 }
 
-contato.addEventListener('keyup', handleKeyUp)
+contato.addEventListener('change', handleChange)
