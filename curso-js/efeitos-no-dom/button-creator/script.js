628faee79b0a85b5cd964db2fa3380bd8d3bb679
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-03 14:46:08 -0300

    aula

--- curso-js/efeitos-no-dom/button-creator/script.js
+++ curso-js/efeitos-no-dom/button-creator/script.js
@@ -49,8 +49,23 @@ function handleChange(event) {
   const value = event.target.value
 
   handleStyle[name](value)
+  saveValues(name, value)
   showCss()
 }
+
+function saveValues(name, value) {
+  localStorage[name] = value
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage)
+  properties.forEach(property => {
+    handleStyle[property](localStorage[property])
+    controles.elements[property].value = localStorage[property]
+  })
+  showCss()
+}
+setValues()
 
 function showCss() {
   cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>')
