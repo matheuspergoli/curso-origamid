750b5a4df501a1a3eb7fa01880399942a0b1fa1c
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-10 03:42:33 -0300

    aula

--- curso-js/javascript-assincrono/fetch/script.js
+++ curso-js/javascript-assincrono/fetch/script.js
@@ -1,12 +1,58 @@
 const sobre = fetch('./sobre.html')
-
 const div = document.createElement('div')
 
 sobre
-.then(resolucao => resolucao.text())
-
+.then(response => response.text())
 .then(body => {
   div.innerHTML = body
-  const titulo = div.querySelector('p')
-  document.querySelector('h1').innerText = titulo.innerText 
+  const titulo = div.querySelector('h1')
+  const texto = div.querySelector('p')
+  document.body.appendChild(titulo)
+  document.body.appendChild(texto)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btn = document.querySelector('button')
+
+// function mostrarCEP() {
+//   const input = document.querySelector('input').value
+//   if (input.length > 8 || input.length === 0) {
+//     alert('Digite um CEP válido.')
+//     return
+//   } else {
+//     const url = `https://viacep.com.br/ws/${input}/json/`
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(dados => {
+//       const bairro = document.querySelector('.bairro')
+//       const cep = document.querySelector('.cep')
+//       const localidade = document.querySelector('.localidade')
+  
+//       bairro.innerText = `Bairro: ${dados.bairro}`
+//       cep.innerText = `CEP: ${dados.cep}`
+//       localidade.innerText = `Localidade: ${dados.localidade}`
+//     })
+//     .catch(() => {
+//       alert(`${input} CEP Não encontrado.`)
+//     })
+//     .finally(() => {
+//       console.log('Busca finalizada')
+//     })
+//   }
+// }
+
+// btn.addEventListener('click', mostrarCEP)
