1709e597bfcbb167a59b15a8592f2983ac64df5d
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-11 17:55:26 -0300

    aula

---
+++ curso-js/javascript-assincrono/api-e-http/script.js
@@ -0,0 +1,28 @@
+// const url = 'https://pokeapi.co/api/v2/pokemon/'
+
+// fetch(url)
+// .then(response => response.json())
+// .then(json => {
+//   console.log(json)
+// })
+
+// const url = 'https://jsonplaceholder.typicode.com/posts/'
+// const options = {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json; charset=utf-8"
+//   },
+//   body: '{ "title": "JavaScript" }'
+// }
+
+// fetch(url, options)
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+// const url = 'https://www.google.com'
+
+// fetch(url)
+// .then(response => response.text())
+// .then(text => {
+//   console.log(text)
+// })
