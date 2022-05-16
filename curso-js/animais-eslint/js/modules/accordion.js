4fdc3cc4b8a653e33d8c437a2f63ec5229d22aa1
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-15 23:06:38 -0300

    aula

--- curso-js/animais-eslint/js/modules/accordion.js
+++ curso-js/animais-eslint/js/modules/accordion.js
@@ -1,16 +1,17 @@
 export default function initAccordion() {
   const accordionList = document.querySelectorAll('[data-anime="accordion"] dt')
   const activeClass = 'ativo'
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass)
-    accordionList[0].nextElementSibling.classList.add(activeClass)
-    accordionList.forEach(item => {
-      item.addEventListener('click', activeAccordion)
-    })
-  }
 
   function activeAccordion() {
     this.classList.toggle(activeClass)
     this.nextElementSibling.classList.toggle(activeClass)
+  }
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion)
+    })
   }
 }
--- curso-js/animais-eslint/js/modules/anima-numeros.js
+++ curso-js/animais-eslint/js/modules/anima-numeros.js
@@ -1,14 +1,14 @@
 export default function initAnimaNumeros() {
   function animaNumeros() {
     const numeros = document.querySelectorAll('[data-numero]')
-    
-    numeros.forEach(numero => {
+
+    numeros.forEach((numero) => {
       const total = +numero.innerText
       const incremento = Math.floor(total / 100)
-    
+
       let start = 0
       const timer = setInterval(() => {
-        start = start + incremento
+        start += incremento
         numero.innerText = start
         if (start > total) {
           numero.innerText = total
@@ -17,15 +17,17 @@ export default function initAnimaNumeros() {
       }, 25 * Math.random())
     })
   }
-  
+
+  const observeTarget = document.querySelector('.numeros')
+
+  let observer
   function handleMutation(mutation) {
     observer.disconnect()
     if (mutation[0].target.classList.contains('ativo')) {
       animaNumeros()
     }
   }
-  
-  const observeTarget = document.querySelector('.numeros')
-  const observer = new MutationObserver(handleMutation)
-  observer.observe(observeTarget, {attributes: true})
+  observer = new MutationObserver(handleMutation)
+
+  observer.observe(observeTarget, { attributes: true })
 }
