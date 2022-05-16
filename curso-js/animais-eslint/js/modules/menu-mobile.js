52913dd5cc0285f76e316445febf55cd179d837f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-15 23:15:06 -0300

    aula

--- curso-js/animais-eslint/js/modules/menu-mobile.js
+++ curso-js/animais-eslint/js/modules/menu-mobile.js
@@ -1,11 +1,11 @@
-import outsideClick from "./outside-click.js"
+import outsideClick from './outside-click.js'
 
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]')
   const menuList = document.querySelector('[data-menu="list"]')
   const eventos = ['click', 'touchstart']
-  
-  function openMenu(event) {
+
+  function openMenu() {
     menuList.classList.add('active')
     menuButton.classList.add('active')
     outsideClick(menuList, eventos, () => {
@@ -13,8 +13,8 @@ export default function initMenuMobile() {
       menuButton.classList.remove('active')
     })
   }
-  
-  eventos.forEach(userEvent => {
+
+  eventos.forEach((userEvent) => {
     menuButton.addEventListener(userEvent, openMenu)
   })
 }
