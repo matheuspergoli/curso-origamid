1a8d9892cc2096cf3693e2129878412878291dfb
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-15 23:20:01 -0300

    aula

--- curso-js/animais-eslint/js/modules/scroll-animacao.js
+++ curso-js/animais-eslint/js/modules/scroll-animacao.js
@@ -2,7 +2,7 @@ export default function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]')
   if (sections.length) {
     const windowMetade = window.innerHeight * 0.4
-    
+
     function animaScroll() {
       sections.forEach(section => {
         const sectionTop = section.getBoundingClientRect().top - windowMetade
