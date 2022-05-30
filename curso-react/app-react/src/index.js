58f52079c19d24375ef3fff58eb458c659748afd
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-30 18:33:25 -0300

    Aula - JSX / Exercício

--- curso-react/app-react/src/index.js
+++ curso-react/app-react/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Compras from './exercicio'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Compras />
   </React.StrictMode>
 );
