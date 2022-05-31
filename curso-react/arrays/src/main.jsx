8d3c1cba0c4dd9b10f4265025884f280d9f2161c
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-31 14:57:14 -0300

    Aula - JSX / Arrays / Exercício

--- curso-react/arrays/src/main.jsx
+++ curso-react/arrays/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import Vendas from './exercicio'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Vendas />
   </React.StrictMode>
 )
