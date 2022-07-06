377c8f0c8b4595c816098ca2c64bc304bf1847fa
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-07-06 13:30:44 -0300

    aula

--- curso-js/slide/js/slide.js
+++ curso-js/slide/js/slide.js
@@ -143,6 +143,12 @@ export class Slide {
 }
 
 export class SlideNav extends Slide {
+
+  constructor(slide, wrapper) {
+    super(slide, wrapper)
+    this.bindControlEvents()
+  }
+
   addArrow(prev, next) {
     this.prevElement = document.querySelector(prev)
     this.nextElement = document.querySelector(next)
@@ -152,5 +158,40 @@ export class SlideNav extends Slide {
   addArrowEvent() {
     this.prevElement.addEventListener('click', this.activePrevSlide)
     this.nextElement.addEventListener('click', this.activeNextSlide)
+  }
+
+  createControl() {
+    const control = document.createElement('ul')
+    control.dataset.control = 'slide'
+    
+    this.slideArray.forEach((item, index) => {
+      control.innerHTML += `<li><a href="#slide${index + 1}">${index}</a></li>`
+    })
+
+    this.wrapper.appendChild(control)
+    return control
+  }
+
+  eventControl(item, index) {
+    item.addEventListener('click', (event) => {
+      event.preventDefault()
+      this.changeSlide(index)
+      this.activeControlItem()
+    })
+  }
+
+  activeControlItem() {
+    this.controlArray.forEach(item => item.classList.remove(this.activeClass))
+    this.controlArray[this.index.active].classList.add(this.activeClass)
+  }
+
+  addControl(customControl) {
+    this.control = document.querySelector(customControl) || this.createControl()
+    this.controlArray = [ ...this.control.children ]
+    this.controlArray.forEach(this.eventControl)
+  }
+
+  bindControlEvents() {
+    this.eventControl = this.eventControl.bind(this)
   }
 }
