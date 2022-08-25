88030aaecaea1a20e27da13a915559e4b1eb6341
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-08-24 23:39:30 -0300

    Aula - Formulários / Checkbox

---
+++ curso-react/checkbox/src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+
+function App() {
+  const [cores, setCores] = React.useState(['azul'])
+
+  function handleChange({target}) {
+    if (target.checked) {
+      setCores([ ...cores, target.value ])
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value))
+    }
+  }
+
+  function checkColor(cor) {
+    return cores.includes(cor)
+  }
+
+  return (
+    <form>
+      <label>
+        <input 
+        type="checkbox" 
+        value="azul" 
+        checked={checkColor('azul')}
+        onChange={handleChange} />
+        Azul
+      </label>
+
+      <label>
+        <input 
+        type="checkbox" 
+        value="vermelho"
+        checked={checkColor('vermelho')}
+        onChange={handleChange} />
+        Vermelho
+      </label>
+    </form>
+  )
+}
+
+export default App
