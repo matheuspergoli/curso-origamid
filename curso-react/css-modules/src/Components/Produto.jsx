db33205fcef21dc0564cbef802ac59620b22d2e7
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-08-27 04:36:33 -0300

    Aula - CSS / Modules

---
+++ curso-react/css-modules/src/Components/Produto.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "./Produto.module.css"
+
+function Produto() {
+
+  return (
+    <div>
+      <h1 className={styles.titulo}>Notebook</h1>
+      <p className={styles.preco}>R$ 20000</p>
+      <button className={styles.comprar}>Comprar</button>
+    </div>
+  )
+}
+
+export default Produto
