30bcc1e771a3aca09bdc1db226477d57bd63f357
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-04 19:04:02 -0300

    useForm

--- curso-react/projeto-dogs/src/Components/Forms/Input/Input.jsx
+++ curso-react/projeto-dogs/src/Components/Forms/Input/Input.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from "./Input.module.css"
 
-function Input({ label, type, name }) {
+function Input({ label, type, name, value, onChange, onBlur, error }) {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>{label}</label>
-      <input className={styles.input} type={type} id={name} name={name} />
-      <p className={styles.error}>Error</p>
+      <input 
+      className={styles.input} 
+      type={type} 
+      id={name} 
+      name={name}
+      value={value}
+      onBlur={onBlur}
+      onChange={onChange} />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   )
 }
