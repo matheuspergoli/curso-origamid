4ff267555d5d1c715520ab8fdd82bf1ae48f4f4a
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-04 03:08:24 -0300

    Estrutura Inicial

--- curso-react/projeto-dogs/src/Components/Header/Header.jsx
+++ curso-react/projeto-dogs/src/Components/Header/Header.jsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom"
+import React from "react"
 import styles from "./Header.module.css"
+import { Link } from "react-router-dom"
+import { ReactComponent as Dogs } from "../../assets/dogs.svg"
 
 function Header() {
   return (
     <div className={styles.header}>
       <nav className="container">
-        <Link to="/">Home</Link>
+        <Link to="/"> 
+          <Dogs />
+        </Link>
         <Link to="/login">Login | Criar</Link>
       </nav>
     </div>
