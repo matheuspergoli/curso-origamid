27da76acbbdade56a14683a58497b6c934901b9e
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-04 22:32:59 -0300

    userContext

--- curso-react/projeto-dogs/src/App.jsx
+++ curso-react/projeto-dogs/src/App.jsx
@@ -5,18 +5,21 @@ import Home from "./Components/Home/Home"
 import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer"
 import Login from "./Components/Login/Login"
+import { UserStorage } from "./Context/UserContext"
 
 function App() {
 
   return (
     <div>
       <BrowserRouter>
+      <UserStorage>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login/*" element={<Login />} />
         </Routes>
         <Footer />
+      </UserStorage>
       </BrowserRouter>
     </div>
   )
--- curso-react/projeto-dogs/src/Components/Header/Header.jsx
+++ curso-react/projeto-dogs/src/Components/Header/Header.jsx
@@ -2,15 +2,22 @@ import React from "react"
 import styles from "./Header.module.css"
 import { Link } from "react-router-dom"
 import DogsImg from "../../assets/dogs.svg"
+import { UserContext } from "../../Context/UserContext"
 
 function Header() {
+  const { data } = React.useContext(UserContext)
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home"> 
           <img src={DogsImg} alt="Logo" />
         </Link>
-        <Link className={styles.login} to="/login">Login | Criar</Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">{data.nome}</Link>
+        ) : (
+          <Link className={styles.login} to="/login">Login | Criar</Link>
+        )}
       </nav>
     </header>
   )
